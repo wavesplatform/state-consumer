3a3ec7a365e3acc3c8096e888ed9c6856a49a530
Artyom Sidorenko <asidorenko@web3tech.ru>
2022-10-18 06:49:11 +0500

    finish metrics

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,9 @@ pub mod schema;
 use anyhow::Result;
 use data_entries::{repo::DataEntriesRepoImpl, updates::DataEntriesSourceImpl};
 use std::sync::Arc;
+use tokio::select;
 use wavesexchange_log::{error, info};
+use wavesexchange_warp::MetricsWarpBuilder;
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -25,16 +27,23 @@ async fn main() -> Result<()> {
 
     info!("Starting state-consumer");
 
-    if let Err(err) = data_entries::daemon::start(
+    let consumer = data_entries::daemon::start(
         updates_repo,
         data_entries_repo,
         config.data_entries.updates_per_request,
         config.data_entries.max_wait_time_in_secs,
-    )
-    .await
-    {
-        error!("{}", err);
-        panic!("{}", err);
-    }
+    );
+
+    let metrics = MetricsWarpBuilder::new()
+        .with_metrics_port(config.metrics_port)
+        .run_blocking();
+
+    select! {
+        Err(err) = consumer => {
+            error!("{}", err);
+            panic!("{}", err);
+        },
+        _ = metrics => error!("metrics stopped")
+    };
     Ok(())
 }
